226a50171746e7950b967d3e8011c12745d76c38
Maxim Moroz <maxim@f1news.ru>
2024-10-13 22:13:45 +0300

    fix token calc

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -232,8 +232,8 @@ def num_tokens_from_messages(messages, model=DEFAULT_OPENAI_MODEL):
     for message in messages:
         num_tokens += 4  # every message follows <im_start>{role/name}\n{content}<im_end>\n
         for key, value in message.items():
-            num_tokens += len(encoding.encode(value))
-            if key == "name":  # if there's a name, the role is omitted
+            num_tokens += len(encoding.encode(value[1]))
+            if key[0] == "name":  # if there's a name, the role is omitted
                 num_tokens += -1  # role is always required and always 1 token
     num_tokens += 2  # every reply is primed with <im_start>assistant
     return num_tokens
